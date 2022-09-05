eac4aab52ca06aebacadd7cad7db3d78c84e3c2a
realariful <real_ariful@hotmail.com>
2022-09-05 14:55:53 +0600

    [FIX] report_download function

--- web_progress/controllers/main.py
+++ web_progress/controllers/main.py
@@ -5,9 +5,17 @@ from odoo.addons.web.controllers.main import ReportController, request
 class WPReportController(ReportController):
 
     @http.route(['/report/download'], type='http', auth="user")
-    def report_download(self, data, token):
+    def report_download(self, data, context=None):
         """
-        Pass web progress code from request content to the context
+            This function is used by 'action_manager_report.js' in order to trigger the download of
+            a pdf/controller report.
+
+            :param data: a javascript array JSON.stringified containg report internal url ([0]) and
+            type [1]
+            :returns: Response with an attachment header
+
+
+            Pass web progress code from request content to the context
         """
         requestcontent = json.loads(data)
         if len(requestcontent) > 2:
@@ -19,6 +27,6 @@ class WPReportController(ReportController):
                 request._context = context
         web_progress_obj = request.env['web.progress']
         web_progress_obj.web_progress_percent(0, 'Report')
-        ret = super(WPReportController, self).report_download(data, token)
+        ret = super(WPReportController, self).report_download(data)
         web_progress_obj.web_progress_percent(100, 'Report done')
         return ret
